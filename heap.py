18d175acad93731d9483a6f771f3e49f88da50a5
timtim1342 <tim.muha@gmail.com>
2022-11-08 23:13:58 +0600

    first commit

---
+++ heap.py
@@ -0,0 +1,156 @@
+from praatio import textgrid
+from functools import wraps
+import os
+
+"""
+В поле глосс не должно быть пустых мест.
+Границы крайних форм должны совпадать с границами предложений.
+"""
+
+LEXEMES = 0
+
+
+class ParseTextGrid:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def file_open(self):
+        return textgrid.openTextgrid(self.filename, includeEmptyIntervals=True)
+
+    def translation(self):
+        tg = self.file_open()
+        translation_tier = tg.tierDict["2"]
+        translation_list = [list(word) for word in translation_tier.entryList]
+        return translation_list
+
+    def Ctranscription(self):
+        tg = self.file_open()
+        transcription_tier = tg.tierDict["1"]
+        transcription_list = [list(word) for word in transcription_tier.entryList]
+        return transcription_list
+
+    def transcription(self):
+        tg = self.file_open()
+        Ctranscription_tier = tg.tierDict["5"]
+        Ctranscription_list = [list(word) for word in Ctranscription_tier.entryList]
+        return Ctranscription_list
+
+class MakeGlossLine(ParseTextGrid):
+
+    def make_line(self):
+        lines = []
+        translation = self.translation()
+        transcription = self.transcription()
+        Ctranscription = self.Ctranscription()
+        for i in range(len(transcription)):
+            transc_line = transcription[i][2]
+            Ctransc_line = Ctranscription[i][2]
+            try:
+                trans_line = translation[i][2]
+            except IndexError:
+                trans_line = 'NO TRANSLATION'
+
+            lines.append((transc_line, Ctransc_line, trans_line))
+
+        return lines
+
+"""
+Starting html construction
+"""
+
+
+def write_html_header(func):
+    @wraps(func)
+    def wrapper():
+        header = """ <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!-- Bootstrap CSS -->
+        <title>Свалка тукитинских текстов</title>
+        <style type="text/css">
+        body {
+            padding: 40px;
+            line-height: 1.5;
+        }
+        .example {
+            padding: 1px; /* Поля */
+            white-space: pre;
+            }
+         p {
+            margin-top: 1em; /* Отступ сверху */
+            margin-bottom: 1em; /* Отступ снизу */
+        }
+        button {
+        background: #f2f6f8; /* Цвет фона */
+        border: 1px solid #7a7b7e; /* Параметры рамки */
+        width: 30px; /* Ширина кнопки */
+        height: 30px; /* Высота */
+        border-radius: 30px;
+        }
+        </style>
+        </head>
+        <body>
+        <header>
+        </header>
+        <br>
+        <br>
+        """
+        with open("text_heap.html", 'a', encoding="UTF-8") as html:
+            html.write(header)
+
+        html.close()
+        func()
+        bottom = """
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        </body>
+        </html>
+        """
+        with open("text_heap.html", 'a', encoding="UTF-8") as html:
+            html.write(bottom)
+        html.close()
+    return wrapper
+
+
+@write_html_header
+def put_texts():
+    for filename in os.listdir('latin_textgrids'):
+        with open("text_heap.html", 'a', encoding="UTF-8") as html:
+            html.write("""<nav class="navbar navbar-expand-lg navbar-light bg-light rounded"><div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample10">
+      <ul class="navbar-nav"><li class="nav-item active"><a class="nav-link" href="#"><h2><b>"""
+                       + filename.split('.')[0])
+            html.write("""\n</b></h2><span class="sr-only">(current)</span></a></li></ul></div></nav>""")
+            html.close()
+        print(filename)
+        parser = MakeGlossLine(os.path.join('latin_textgrids', filename))
+
+        for el in parser.make_line():
+            with open("text_heap.html", 'a', encoding="UTF-8") as html:
+                html.write(el[0] + '\n' + '<br>')
+                html.write(el[1] + '\n')
+                #html.write("\n\n<i><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\' " + el[0] + " \'</p></i><hr>\n\n")
+                #html.write("\n\n<i><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\' " + el[1] + " \'</p></i><hr>\n\n")
+                if el[2] is not None:
+                    html.write("\n\n<i><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\' " + el[2] + " \'</p></i><hr>\n\n")
+            html.close()
+        with open("text_heap.html", 'a', encoding="UTF-8") as html:
+            html.write("""<br><br><br>""")
+            html.close()
+
+
+def main():
+    try:
+        os.remove("text_heap.html")
+    except Exception:
+        pass
+
+    put_texts()
+    print("\nКолчество лексем в копрусе: " + str(LEXEMES))
+
+
+if __name__ == "__main__":
+    main()
---
+++ transcription_layer.py
@@ -0,0 +1,117 @@
+from os import listdir
+from os.path import join
+from praatio import textgrid
+from praatio.praatio_scripts import alignBoundariesAcrossTiers
+
+
+def tg2tg_obj(file_name):
+    tg = textgrid.openTextgrid(file_name, includeEmptyIntervals=True)
+    return tg
+
+
+def tg_obj2tg(tg, file_name):
+    tg.save(file_name, format="short_textgrid", includeBlankSpaces=True)
+
+
+def align_tg(file_name):
+    tg_obj2tg(alignBoundariesAcrossTiers(file_name, maxDifference=0.2), file_name)
+
+
+def replace_blank_translation(file_name):  # new_version to rewrite
+    """
+    Without this translation will move in the heap.
+    Write NULL where there is smth in transcription, but corresponding translation is empty.
+    """
+    tg = tg2tg_obj(file_name)
+    transcription_entries = tg.tierDict['1'].entryList
+    translation_entries = tg.tierDict['2'].entryList
+    new_translation_entries = [(start, stop, label) for start, stop, label in translation_entries]
+    translation = tg.removeTier('2')
+
+    for start_tc, stop_tc, label_tc in transcription_entries:
+        i = 0
+        translation_found = False
+
+        for start_tl, stop_tl, label_tl in translation_entries:
+            if start_tc == start_tl and stop_tc == stop_tl:
+                translation_found = True
+                if label_tl == '' and len(label_tc) > 0:
+                    new_translation_entries[i] = (start_tl, stop_tl, 'NULL')
+                break
+            i += 1
+
+        if not translation_found:
+            raise IndexError(label_tc)
+
+    new_translation_entries.sort(key=lambda x: x[0])
+
+
+    new_translation = translation.new(name='2', entryList=new_translation_entries)
+    tg.addTier(new_translation)
+    tg_obj2tg(tg, file_name)
+
+
+def translit_dict():  # old_version to rewrite
+    with open('transl_dict.csv', 'r', encoding='UTF-8') as f:
+        txt = f.read()
+    txt_list = txt.split('\n')
+    txt_list = [i.split(';') for i in txt_list]
+    translit_dict = {i[0]: i[1] for i in txt_list if len(i) == 2 and i[0] != ''}
+
+    translit_dict_cap = {}
+    for key in translit_dict.keys(): #  ad capitals
+        translit_dict_cap[key.capitalize()] = translit_dict[key].capitalize()
+    translit_dict.update(translit_dict_cap)
+
+    return translit_dict
+
+
+def transliterator(word, transliteration_dict):
+    for sym in transliteration_dict.keys():
+        word = word.replace(sym, transliteration_dict[sym])
+    return word
+
+
+def translitirated_tg(tg):
+    transliteration_dict = translit_dict()
+    transcription = tg.tierDict['1']
+
+    transcription_lat_entries = [(start, stop, ' '.join([transliterator(word, transliteration_dict) for word in label.split()]))
+                                 for start, stop, label in transcription.entryList]
+
+    transcription_lat = transcription.new(name='5', entryList=transcription_lat_entries)
+    tg.addTier(transcription_lat)
+    return tg
+
+
+def translit_all():
+    input_dir, output_dir = 'cyrillic_textgrids', 'latin_textgrids'
+
+    for tg_name in listdir(input_dir):
+        tg_input_path, tg_output_path = join(input_dir, tg_name), join(output_dir, tg_name)
+
+        try:
+            tg = tg2tg_obj(tg_input_path)
+            new_tg = translitirated_tg(tg)
+            tg_obj2tg(new_tg, tg_output_path)
+
+        except Exception as ex:
+            print(f' Error when transliterating {tg_name}: {ex}')
+
+        try:
+            align_tg(tg_output_path)
+
+        except Exception as ex:
+            print(f' Error when aligning {tg_name}: {ex}')
+
+        try:
+            replace_blank_translation(tg_output_path)
+
+        except Exception as ex:
+            print(f' Error when replacing empty translation {tg_name}: {ex}')
+
+def main():
+    translit_all()
+
+if __name__ == '__main__':
+    main()
